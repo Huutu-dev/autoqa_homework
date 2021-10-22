d0d768939d7398525a2b3d054abe0a0afe91e2d9
Huutu-dev <lehuutu.dev@gmail.com>
2021-10-21 21:07:19 -0700

    Chrome driver 95

--- web_driver.py
+++ web_driver.py
@@ -2,7 +2,7 @@ from functools import partial
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-chromedriver_path = r"D:\Careerist\AutomaticQA\bin\chromedriver.exe"
+chromedriver_path = r"D:\Careerist\AutomaticQA\bin\Chrome95\chromedriver.exe"
 chrome_driver = partial(webdriver.Chrome, service=Service(chromedriver_path))
 
 __all__ = ["chrome_driver"]
